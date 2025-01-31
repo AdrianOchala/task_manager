22e17fc7b39cf8b636b083b3f3beb0c485e4d4c2
Adrian Ochała <ochala.adrian@gmail.com>
2025-01-31 22:55:08 +0100

    feat: vue config files, laravel auth views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:scripts>
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </x-slot:scripts>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', static fn () => view('login'))->name('login');
+    Route::get('/register', static fn () => view('register'))->name('register');
 });
+
+Route::middleware('auth')->get('/', static fn () => view('home'))->name('home');
