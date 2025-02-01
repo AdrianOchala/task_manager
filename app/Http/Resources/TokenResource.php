d87c476e99867a035157cdac2c443143aa2e0c5f
Adrian Ochała <ochala.adrian@gmail.com>
2025-02-02 00:07:55 +0100

    fix: missing phpdoc token resource

--- app/Http/Resources/TokenResource.php
+++ app/Http/Resources/TokenResource.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Laravel\Sanctum\NewAccessToken;
 
+/**
+ * @mixin NewAccessToken
+ */
 class TokenResource extends JsonResource
 {
     public function toArray($request): array
